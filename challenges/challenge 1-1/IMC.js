1d89a325f0796f21285acdf9d8d6052047056734
João Marcos <mark17primo@gmail.com>
2020-10-15 23:53:08 -0300

    adjustment

---
+++ challenges/challenge 1-1/IMC.js
@@ -0,0 +1,12 @@
+const name = "Carlos"
+const weight = 84
+const height = 1.88
+
+const imc = height / (weight * weight)
+
+if(imc >= 30) {
+    console.log(`Carlos you're overweight: ${imc}`)
+}
+else {
+    console.log(`Carlos you're not overweight: ${imc}`)
+}
