918e860a8dd7cd79903e6783fd0c5db35eee65a6
João Marcos <mark17primo@gmail.com>
2020-10-15 23:48:49 -0300

    challange 1-1 finished

---
+++ challenges/Retirement.js
@@ -0,0 +1,16 @@
+const name = "Silvana"
+const sex = "F"
+const age = 48
+const contribution = 23
+
+const contributionCalc = age + contribution
+
+const manCanRetire = sex == 'M' && contribution >= 35 && contributionCalc >= 95
+const womanCanRetire = sex == 'F' && contribution >= 30 && contributionCalc >= 85
+
+if(manCanRetire || womanCanRetire) {
+    console.log(`${name}, you can retire.`)
+}
+else {
+    console.log(`${name}, you can't retire.`)
+}
