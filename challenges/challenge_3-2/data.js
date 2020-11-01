2cf049316db545534b4dd2206659191145f8ce50
João Marcos <mark45.primo@gmail.com>
2020-11-01 18:07:31 -0300

    data script

---
+++ challenges/challenge_3-2/data.js
@@ -0,0 +1,29 @@
+module.exports = [
+    {
+        id: "https://app.rocketseat.com.br/starter",
+        logo_url: "starter.png",
+        title: "Starter",
+        type: "Courses",
+        description: "<strong>Become a desired programmer</strong> on the market with this free courses",
+        button: '',
+        css: "card__starter"
+    },
+    {
+        id: "https://app.rocketseat.com.br/dashboard/launchbase",
+        logo_url: "launchbase.png",
+        title: "Launchbase",
+        type: "Bootcamp",
+        description: "<strong>Domain the programming scratch</strong> and have access to the best market opportunities",
+        button: '',
+        css: "card__launchbase"
+    },
+    {
+        id: "https://pages.rocketseat.com.br/gostack",
+        logo_url: "gostack.png",
+        title: "GoStack",
+        type: "Bootcamp",
+        description: "<strong>Immersive training</strong> in the most modern web and mobile development technologies",
+        button: '<a href="#" class="card__button" id=""><strong>Guarantee your place</strong> for the next class!</a>',
+        css: "card__gostack"
+    }
+]
