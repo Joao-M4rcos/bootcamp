311fa8a49d94593bc26a219a4a58e70cb8ad2b4c
João <mark45.primo@gmail.com>
2021-02-28 18:54:10 -0300

    edit and delete done

--- modules/module-4/instructors.js
+++ modules/module-4/instructors.js
@@ -19,7 +19,7 @@ exports.show = (req, res) => {
         services: foundInstructor.services.split(","),
         created_at: new Intl.DateTimeFormat().format(foundInstructor.created_at),
     }
-    
+
     return res.render('instructors/show', { instructor })
 }
 
@@ -73,5 +73,52 @@ exports.edit = (req, res) => {
         birth: date(foundInstructor.birth)
     }
 
-    return res.render('instructors/edit', { instructor: instructor })
+    return res.render('instructors/edit', { instructor })
+}
+
+// PUT
+exports.put = (req, res) => {
+    const { id } = req.body
+    let index = 0
+
+    const foundInstructor = data.instructors.find((instructor, foundIndex) => {
+        if (id == instructor.id) {
+            index = foundIndex
+            return true
+        }
+    })
+
+    if (!foundInstructor) return res.send("Instructor not found!")
+
+    const instructor = {
+        ...foundInstructor,
+        ...req.body,
+        birth: Date.parse(req.body.birth),
+    }
+
+    data.instructors[index] = instructor
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
+        if (err) return res.send("Write error!")
+
+        return res.redirect(`/instructors/${id}`)
+    }) 
+
+}
+
+// DELETE
+exports.delete = (req, res) => {
+    const { id } = req.body
+
+    const filteredInstructor = data.instructors.filter((instructor) => {
+        return instructor.id != id
+    })
+
+    data.instructors = filteredInstructor
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
+        if (err) res.send("Write file error!")
+
+        return res.redirect("/instructors")
+    })
 }
