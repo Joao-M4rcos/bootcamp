626b1438a104f1e8fe3dafd4274327b4302c34d5
João <mark45.primo@gmail.com>
2021-02-28 15:18:01 -0300

    module04 in progress

--- modules/module-4/instructors.js
+++ modules/module-4/instructors.js
@@ -1,26 +1,49 @@
 const fs = require('fs')
 const data = require('./data.json')
+const { age, date } = require('./utils')
+
+// SHOW
+exports.show = (req, res) => {
+
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find((instructor) => {
+        return instructor.id == id
+    })
+
+    if (!foundInstructor) return res.send("Instructor not found!")
+
+    const instructor = {
+        ...foundInstructor,
+        age: age(foundInstructor.birth),
+        services: foundInstructor.services.split(","),
+        created_at: new Intl.DateTimeFormat().format(foundInstructor.created_at),
+    }
+    
+    return res.render('instructors/show', { instructor })
+}
+
 // CREATE
 exports.post = (req, res) => {
 
     const keys = Object.keys(req.body)
 
-    for(key of keys) {
+    for (key of keys) {
         if (req.body[key] == "") return res.send("Please, fill all fields!")
     }
 
-    let { avatar_url, name, birth, gender, service } = req.body
+    let { avatar_url, name, birth, gender, services } = req.body
 
     birth = Date.parse(birth)
     const created_at = Date.now()
     const id = Number(data.instructors.length + 1)
 
     data.instructors.push({
-        id, 
-        avatar_url, 
-        name, birth, 
-        gender, 
-        service, 
+        id,
+        avatar_url,
+        name, birth,
+        gender,
+        services,
         created_at
     })
 
@@ -34,6 +57,21 @@ exports.post = (req, res) => {
 
 }
 
-// UPDATE
+// EDIT
+exports.edit = (req, res) => {
 
-//DELETE
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find((instructor) => {
+        return instructor.id == id
+    })
+
+    if (!foundInstructor) return res.send("Instructor not found!")
+
+    const instructor = {
+        ...foundInstructor,
+        birth: date(foundInstructor.birth)
+    }
+
+    return res.render('instructors/edit', { instructor: instructor })
+}
