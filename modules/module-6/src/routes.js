c5eda6bc2f46e1472a8f45c2e8481a881962a08e
João <mark45.primo@gmail.com>
2021-03-04 01:49:41 -0300

    module06 almost done

---
+++ modules/module-6/src/routes.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const routes = express.Router()
+const ProductController = require("./app/controllers/ProductController")
+
+routes.get('/', (req, res) => {
+    return res.render("layout.njk")
+})
+
+routes.get('/products/create', ProductController.create)
+routes.post('/products', ProductController.post)
+
+routes.get('/products/:id/edit', ProductController.edit)
+routes.put('/products', ProductController.put)
+routes.delete('/products', ProductController.delete)
+
+// Alias
+routes.get('/ads/create', (req, res) => {
+    return res.redirect("/products/create")
+})
+
+module.exports = routes
